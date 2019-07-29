ea43b1db1ff1edf4353d740f9e4cb9e45b4e0793
AndiiBintang <andibintang.a500f@gmail.com>
2019-07-29 19:36:24 +0800

    Add categories tag button layout.

--- lib/widget/widget_textfieldicon.dart
+++ lib/widget/widget_textfieldicon.dart
@@ -1,22 +1,92 @@
 import 'package:flutter/material.dart';
 
 class TextFieldIcon extends StatelessWidget {
-
-
   Widget build(BuildContext context) {
-      return Container(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
-          child: TextField(
-            decoration: InputDecoration(
-              prefixIcon: Icon(Icons.search),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20.0),
-                borderSide: BorderSide(style: BorderStyle.solid)
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: 40,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Container(
+                    height: 10,
+                    padding: const EdgeInsets.all(8),
+                    decoration: new BoxDecoration(
+                      border:
+                          new Border.all(color: Color(0xffed5565), width: 2),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: RawMaterialButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Makanan",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                        textAlign: TextAlign.center,
+                      ),
+                      shape: RoundedRectangleBorder(),
+                      elevation: 2.0,
+                      padding: const EdgeInsets.all(10.0),
+                    ),
+                  ),
+                  Container(
+                    height: 10,
+                    padding: const EdgeInsets.all(8),
+                    decoration: new BoxDecoration(
+                      border:
+                          new Border.all(color: Color(0xffed5565), width: 2),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: RawMaterialButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Minuman",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                        textAlign: TextAlign.center,
+                      ),
+                      shape: RoundedRectangleBorder(),
+                      elevation: 2.0,
+                      padding: const EdgeInsets.all(10.0),
+                    ),
+                  ),
+                  Container(
+                    height: 10,
+                    padding: const EdgeInsets.all(8),
+                    decoration: new BoxDecoration(
+                      border:
+                          new Border.all(color: Color(0xffed5565), width: 2),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: RawMaterialButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Alat Kebersihan",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                        textAlign: TextAlign.center,
+                      ),
+                      shape: RoundedRectangleBorder(),
+                      elevation: 2.0,
+                      padding: const EdgeInsets.all(10.0),
+                    ),
+                  ),
+                ],
               ),
-          ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: BorderSide(style: BorderStyle.solid)),
+              ),
+            ),
+          ],
         ),
-      )
+      ),
     );
   }
 }
