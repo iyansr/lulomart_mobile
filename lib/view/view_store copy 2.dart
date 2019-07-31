1287cdb9d50d2107e680e57141587b9a4f5c483e
muhamaddanil <muhammaddanil790@gmail.com>
2019-07-31 01:15:35 -0700

    gagal

--- lib/view/view_store copy 2.dart
+++ lib/view/view_store copy 2.dart
@@ -5,9 +5,15 @@ import 'package:http/http.dart' as http;
 import 'package:lulomart_mobile/config/api.dart';
 import 'package:lulomart_mobile/widget/widget_textfieldicon.dart';
 
-String ket = "pdctg20181113123027471310";
+var ket = "";
 
 class StorePage extends StatefulWidget {
+
+  addReceip(ReceiptCategory receipt) {
+    storePageLayout.addReceipcategory(receipt);
+  }
+  var storePageLayout = StorePageLayout();
+  
   final ValueChanged<Item> onListItemTap;
   StorePage({this.onListItemTap});
   @override
@@ -22,16 +28,24 @@ class StorePageLayout extends State<StorePage> {
   StorePageLayout({this.onTap});
   List<ItemCard> itemCard = List();
 
+  List<ReceiptcardCategory> receiptTable = List();
+  void addReceipcategory(ReceiptCategory _receipt) {
+    setState(() {
+      // debugPrint("Receipt : " + receipt.name);
+      this.receiptTable.add(new ReceiptcardCategory(receipt: _receipt));
+    });
+  }
+
   getData() async {
     Api api = Api();
 
     return http.get(api.item).then((http.Response response) {
       var datauser = json.decode(response.body);
-      debugPrint(datauser[0]["productcategory_id"]);
+      // debugPrint(datauser[0]["product_picture"]);
       List<ItemCard> items = List();
       
       for (var i = 0; i < datauser.length; i++) {
-        datauser[i]['productcategory_id'] == ket ?
+        if (datauser[i]['productcategory_id'] == ket) {
           items.add(
             new ItemCard(
               onTap: (Item item) {
@@ -44,10 +58,23 @@ class StorePageLayout extends State<StorePage> {
                 productPrice: datauser[i]['product_price'],
               ),
             ),
-          ) :
-          Text("Kosong");
-          // Text("gagal")
-
+          );
+        } else {
+          items.add(
+            new ItemCard(
+              onTap: (Item item) {
+                // debugPrint("${item.productPrice}");
+                onTap(item);
+              },
+              item: new Item(
+                productPicture: datauser[i]['product_picture'],
+                productName: datauser[i]['product_name'],
+                productPrice: datauser[i]['product_price'],
+              ),
+            ),
+          );
+          // Text("gagal");
+        }
       }
 
       setState(() {
@@ -61,7 +88,7 @@ class StorePageLayout extends State<StorePage> {
     getData();
     super.initState();
   }
-
+  var storage = StorePageLayout();
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
@@ -78,12 +105,12 @@ class StorePageLayout extends State<StorePage> {
               padding: const EdgeInsets.all(1.0),
               child: Container(
                 child: TextFieldIcon(
-                    // onListItemTapCategory: (ItemCategory item) {
-                    //   debugPrint("StorePage : " + item.productcategoryId);
-                    //   onListItemTapCategory.addReceip(new ReceiptCategory(
-                    //     name: item.productcategoryId,
-                    //   ));
-                    // },
+                    onListItemTapCategory: (ItemCategory item) {
+                      debugPrint("StorePage : " + item.productcategoryId);
+                      storage.addReceipcategory(new ReceiptCategory(
+                        name: item.productcategoryId,
+                      ));
+                    },
                     ),
               ),
             ),
@@ -342,19 +369,22 @@ class Receiptcard extends StatelessWidget {
 }
 
 class TextFieldIcon extends StatefulWidget {
+  
   final ValueChanged<ItemCategory> onListItemTapCategory;
   TextFieldIcon({this.onListItemTapCategory});
   @override
-  _TextFieldIconState createState() =>
-      _TextFieldIconState(onTap: (ItemCategory item) {
-        // debugPrint("StorePageitem list : " + item.user);
-        this.onListItemTapCategory(item);
-      });
+  _TextFieldIconState createState() => _TextFieldIconState();
+      // onListItemTapCategory: (ItemCategory item) {
+      //   debugPrint("StorePage : " + item.productcategoryId);
+      //   rightDrawer.addReceip(new Receipt(
+      //     name: item.productcategoryId,
+      //   ));
+      // },
 }
 
 class _TextFieldIconState extends State<TextFieldIcon> {
-  final ValueChanged<ItemCategory> onTap;
-  _TextFieldIconState({this.onTap});
+  final ValueChanged<ItemCategory> onListItemTapCategory;
+  _TextFieldIconState({this.onListItemTapCategory});
   List<ItemCardCategory> itemCard = List();
 
   getData() async {
@@ -370,7 +400,7 @@ class _TextFieldIconState extends State<TextFieldIcon> {
           new ItemCardCategory(
             onTap: (ItemCategory item) {
               // debugPrint("${item.productPrice}");
-              onTap(item);
+              onListItemTapCategory(item);
             },
             item: new ItemCategory(
               productcategoryname: datauser[i]['productcategory_name'],
@@ -455,9 +485,9 @@ class _ItemCardCategoryState extends State<ItemCardCategory> {
             setState(() {
               ket = widget.item.productcategoryId;
             });
-            
-            // ket = widget.item.productcategoryId;
-            debugPrint(ket);
+
+            ket = widget.item.productcategoryId;
+            debugPrint(widget.item.productcategoryId);
           },
           child: Text(
             widget.item.productcategoryname,
@@ -484,4 +514,15 @@ class ReceiptCategory {
   final String name;
 
   ReceiptCategory({this.name});
+}
+
+class ReceiptcardCategory extends StatelessWidget {
+  final ReceiptCategory receipt;
+  final Item item;
+
+  const ReceiptcardCategory({Key key, this.receipt, this.item}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Text("data");
+  }
 }
